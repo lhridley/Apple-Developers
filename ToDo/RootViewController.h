b0e732bc872cfb7569a56a6edd0412b07205dc3b
Lisa Ridley <lhridley@gmail.com>
2011-05-26 15:40:11 -0500

    initial code commit of work done on 2011 May 19

---
+++ ToDo/RootViewController.h
@@ -0,0 +1,20 @@
+//
+//  RootViewController.h
+//  ToDo
+//
+//  Created by Lisa Ridley on 5/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ ToDo/ToDoAppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  ToDoAppDelegate.h
+//  ToDo
+//
+//  Created by Lisa Ridley on 5/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CommonCrypto/CommonHMAC.h>
+
+@interface ToDoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
